377157659c2da04dddafaa9a426f36ea47620c80
Orestis Vantzos <o.vantzos@admie.gr>
2026-01-19 15:04:54 +0200

    added back the code in the handler

--- actions/analyzer/ryax_handler.py
+++ actions/analyzer/ryax_handler.py
@@ -1,8 +1,9 @@
 import json
 from pathlib import Path
+import pandas as pd
 
 def handle(request: dict)->dict:
     data_file = Path(request["data"])
-    data = [1,2,3]
+    data = pd.read_parquet(data_file)
     results = {"rows":len(data)}
     return {"results":json.dumps(results)}
