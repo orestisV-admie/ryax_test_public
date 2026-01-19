1d4f669ac487ac155373213803381a5029be0e67
Orestis Vantzos <o.vantzos@admie.gr>
2026-01-19 13:49:18 +0200

    Initialise Analyzer Action

---
+++ actions/analyzer/README.md
@@ -0,0 +1,5 @@
+# Analyzer Action
+
+**Input:** .parquet data file 
+
+**Output:** stringified json with the results
---
+++ actions/analyzer/ryax_handler.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import json
+from pathlib import Path
+
+def handle(request: dict)->dict:
+    data_file = Path(request["data"])
+    data = pd.read_parquet(data_file)
+    results = {"rows":len(data)}
+    return {"results":json.dumps(results)}
