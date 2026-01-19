805ec1d2a6b440095870c3d8dc6bef4bfbd506c1
Orestis Vantzos <o.vantzos@admie.gr>
2026-01-19 14:05:50 +0200

    dummy data

--- actions/analyzer/ryax_handler.py
+++ actions/analyzer/ryax_handler.py
@@ -1,9 +1,10 @@
-import pandas as pd
+# import pandas as pd
 import json
 from pathlib import Path
 
 def handle(request: dict)->dict:
     data_file = Path(request["data"])
-    data = pd.read_parquet(data_file)
+    # data = pd.read_parquet(data_file)
+    data = [1,2,3]
     results = {"rows":len(data)}
     return {"results":json.dumps(results)}
