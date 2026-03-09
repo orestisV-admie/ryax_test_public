fa85008ff3d69b38a247cc292caec4937124ca35
Orestis Vantzos <o.vantzos@admie.gr>
2026-03-09 15:22:39 +0200

    windowmaker handler

--- actions/windowmaker/ryax_handler.py
+++ actions/windowmaker/ryax_handler.py
@@ -3,7 +3,18 @@ from pathlib import Path
 import pandas as pd
 
 def handle(request: dict)->dict:
+    # Parameters
+    win_len, win_step = 50,10
+    
+    # Create the output directory
+    out_dir = Path("/tmp/windows")
+    out_dir.mkdir(exist_ok=True)
+
+    # Scan the file and output the window .parquet
     data_file = Path(request["data"])
     data = pd.read_parquet(data_file)
-    results = {"rows":len(data)}
-    return {"results":json.dumps(results)}
+    for n,t in enumerate(data.rolling(win_len, step=win_step, center=False)):
+        if len(t)==win_len:
+            t.to_parquet(out_dir.joinpath(f"window_{n}.parquet"))
+
+    return {"windows":out_dir.absolute().as_posix()}
