14cc489dded4d09100b31632879dce61003aabb4
Orestis Vantzos <o.vantzos@admie.gr>
2026-03-09 15:46:35 +0200

    analyzer + collater

--- actions/analyzer/ryax_handler.py
+++ actions/analyzer/ryax_handler.py
@@ -1,9 +1,24 @@
 import json
 from pathlib import Path
 import pandas as pd
+from random import random
+
+def analyse(df:pd.DataFrame)->dict:
+    # Dummy 'analysis'
+    anomalous = random()<.01
+    return {"start":df.index.start, "end":df.index.end, "anomalous":anomalous}
 
 def handle(request: dict)->dict:
-    data_file = Path(request["data"])
-    data = pd.read_parquet(data_file)
-    results = {"rows":len(data)}
-    return {"results":json.dumps(results)}
+    # Get the directory with window .parquet
+    in_dir = Path(request["windows"])
+
+    # Analyse the files
+    out_dir = Path("/tmp/results")
+    out_dir.mkdir(exist_ok=True)
+    for file in in_dir.glob("*.parquet"):
+        df = pd.read_parquet(file)
+        res = analyse(df)
+        with open(out_dir.joinpath(f"{file.with_stem}.json"), "w") as jfile:
+            json.dump(res, jfile)
+    
+    return {"results":out_dir.absolute().as_posix()}
--- actions/collator/ryax_handler.py
+++ actions/collator/ryax_handler.py
@@ -3,7 +3,15 @@ from pathlib import Path
 import pandas as pd
 
 def handle(request: dict)->dict:
-    data_file = Path(request["data"])
-    data = pd.read_parquet(data_file)
-    results = {"rows":len(data)}
-    return {"results":json.dumps(results)}
+    # Get the directory with the results .json
+    in_dir = Path(request["results"])
+
+    # Collect the results in a list
+    results = []
+    for fname in in_dir.glob("*.json"):
+        with open(fname, "r") as resf:
+            res:dict = json.load(resf)
+            if res["anomalous"]:
+                results.append(res)
+    
+    return {"report":json.dumps(results)}
